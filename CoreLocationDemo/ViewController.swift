c5ea743628a751792ac708f5b7e3caf4e7f42fc7
Nick Servidio <servidionicholas@gmail.com>
2016-01-02 16:16:03 -0500

    Initial Commit

---
+++ CoreLocationDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreLocationDemo
+//
+//  Created by Nicholas Servidio on 1/2/16.
+//  Copyright © 2016 Intrepid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
